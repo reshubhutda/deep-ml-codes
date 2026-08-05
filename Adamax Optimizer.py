252b2d5369d5030f259d71c08ea7589fb09dff2c
Reshu Bhutda <reshubhutra123@gmail.com>
2026-08-05 02:14:59 -0500

    Adamax Optimizer

---
+++ Adamax Optimizer.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def adamax_optimizer(parameter, grad, m, u, t, learning_rate=0.002, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    """
+    Update parameters using the Adamax optimizer.
+    Adamax is a variant of Adam based on the infinity norm.
+    It uses the maximum of past squared gradients instead of the exponential moving average.
+    Args:
+        parameter: Current parameter value
+        grad: Current gradient
+        m: First moment estimate
+        u: Infinity norm estimate
+        t: Current timestep
+        learning_rate: Learning rate (default=0.002)
+        beta1: First moment decay rate (default=0.9)
+        beta2: Infinity norm decay rate (default=0.999)
+        epsilon: Small constant for numerical stability (default=1e-8)
+    Returns:
+        tuple: (updated_parameter, updated_m, updated_u)
+    """
+	# Your code here
+    parameter = np.array(parameter, dtype=float, ndmin=1)
+    grad = np.array(grad, dtype=float, ndmin=1)
+    m = np.array(m, dtype=float, ndmin=1)
+    u = np.array(u, dtype=float, ndmin=1)
+
+    for i in range(parameter.size):
+        m[i] = beta1 * m[i] + (1 - beta1) * grad[i]
+        u[i] = max(beta2 * u[i], abs(grad[i]))
+
+        parameter[i] -= (
+            learning_rate / (1 - beta1**t)
+        ) * (m[i] / (u[i] + epsilon))
+    return np.round(parameter, 5), np.round(m, 5), np.round(u, 5)
